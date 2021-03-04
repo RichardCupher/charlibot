d8b27381912ebda44dda7c5ba982012771360795
RichardCupher <79860565+RichardCupher@users.noreply.github.com>
2021-03-04 19:40:08 +0500

    Add files via upload

--- chat-bot.py
+++ chat-bot.py
@@ -1,7 +1,11 @@
 #я чарли
 import discord
+from discord.utils import get
 import random
 import os
+import youtube_dl
+yrlll = 'https://youtu.be/F4wIbPq6euM'
+smd = ''
 regt = ''
 namer=''
 msg = ''
@@ -24,6 +28,8 @@ def otv(sms1, otvet):
     for q in otvet:
         if q in sms1:
             return True
+def smds():
+    smd = ''
 @client.event
 async def on_message(message):
     regt = str.lower(message.content)
@@ -34,7 +40,8 @@ async def on_message(message):
         return
     if 'чарли' in regt:
         global namer
-        msg = msg + redactname(str(message.author)) + ' '
+        if 'leave' in regt == False or 'join' in regt == False:
+            msg = msg + redactname(str(message.author)) + ' '
         if otv(regt, priv):
             msg = msg + random.choice(privOtv) + '. '
         if 'кто ты' in regt:
@@ -50,8 +57,62 @@ async def on_message(message):
         if 'пзц' in regt:
             msg = msg + 'Согласен' + '. '
         print(message.author.id)
-        await message.channel.send(msg)
-        msg = ''
-        namer = ''
+        if msg != '':
+            await message.channel.send(msg)
+    else:
+        global voice
+        if 'join' in regt:
+            channel = message.author.voice.channel
+            voice = get(client.voice_clients, guild = message.guild)
+            print('444555')
+            if voice and voice.is_connected():
+                await voice.move_to(channel)
+            else:
+                voice = await channel.connect()
+                await message.channel.send(f'Чарли присоединился к каналу: {channel}')
+        if 'leave' in regt:
+            channel = message.author.voice.channel
+            voice = get(client.voice_clients,guild = message.guild)
+            if voice and voice.is_connected():
+                await voice.disconnect()
+                await message.channel.send(f'Чарли отключился от канала: {channel}')
+        if 'play' in regt:
+            song_there = os.path.isfile('song.mp3')
+            try:
+                if song_there:
+                    os.remove('song.mp3')
+                    print('[log] Старый фаил удалён')
+            except PermissionError:
+                print('[log] не удалось удалить фаил')
+
+            await message.channel.send('Пожалуйста ожидайте')
+            voice = get(client.voice_clients, guild = message.guild)
+            print(voice)
+            ydl_opts = {
+                'format' : 'bestaudio/best',
+                'postprocessors' : [{
+                        'key' : 'FFmpegExtractAudio',
+                        'preferredcodec' : 'mp3',
+                        'preferredquality' : '192'
+                    }]
+                }
+            with youtube_dl.YoutubeDL({}) as ydl:
+                print('[log] Загружаем музыку...')
+                ydl.download([yrlll])
+            for file in os.listdir('./'):
+                print(file)
+                if file.endswith('.mp4') or file.endswith('.webm')or file.endswith('.mkv') :
+                    namemp3 = file
+                    print(f'[log] переименовываю фаил: {file}')
+                    os.rename(file, 'song.mp3')
+                    print(file)
+                    
+            voice.play(discord.FFmpegPCMAudio('song.mp3'), after = lambda e: print(f'[log] {namemp3}, музыка закончила своё проигрывание'))
+            voice.source = discord.PCMVolumeTransformer(voice.source)
+            vi=voice.source.volume = 0.07
+            song_name = namemp3.rsplit('-', 2)
+            await message.channel.send(f'Сейчас играет: {song_name[0]}')
+    msg = ''
+    namer = ''
 token = os.environ.get('tokencharli')
 client.run(str(token))
